aa53e8d69a7c92894dc4fa820f0c80de8f5c6fe5
aamir <wasi3aamir@gmail.com>
2019-01-28 08:40:07 +0600

    Accepted

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for(int t = 0; t < T; t++)
+            {
+                string line = Console.ReadLine();
+                string[] space = line.Split(' ');
+                int a = int.Parse(space[0]);
+                int b = int.Parse(space[1]);
+                int Ab = int.Parse(space[2]);
+                int An = a - Ab;
+                int Ba = b - An;
+                Console.WriteLine(Ba);
+            }
+        }
+    }
+}
